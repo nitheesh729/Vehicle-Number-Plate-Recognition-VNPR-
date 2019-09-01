bd7813bd5b4dbb8e73ff5f40e82c266dfa8b7e81
nitheesh729 <54501666+nitheesh729@users.noreply.github.com>
2019-09-01 18:04:58 +0530

    Add files via upload

---
+++ VNPR.py
@@ -0,0 +1,44 @@
+import pytesseract
+import numpy as np
+import pandas as pd
+from PIL import Image
+import json
+import urllib
+import cv2
+
+def ExtractNumbers(List):
+    for index,Row in List.iterrows():
+        Response = urllib.request.urlopen(Row[0])
+        Img = np.array(Image.open(Response))
+        Images.append(Img)
+        xt = Row[1][0]['x']*Img.shape[1]
+        yt = Row[1][0]['y']*Img.shape[0]
+        xb = Row[1][1]['x']*Img.shape[1]
+        yb = Row[1][1]['y']*Img.shape[0]
+        fullImage = Image.fromarray(Img)
+        plateImage = fullImage.crop((xt,yt,xb,yb))
+        Plates.append(np.array(plateImage))
+        plateImage.save('image.png')
+        img=cv2.imread('image.png')
+        img=cv2.resize(img,(int(img.shape[1]*4),int(img.shape[0]*4)))
+        Num=pytesseract.image_to_string(img)
+        print(Num)
+        Numbers.append(Num) 
+Images =[] 
+Plates =[] 
+Numbers=[]
+
+if __name__== "__main__":
+
+    List = pd.read_json('Indian_Number_plates.json', lines=True)
+    pd.set_option('display.max_colwidth', -1)
+    del List['extras']
+    List['points'] = List.apply(lambda Row: Row['annotation'][0]['points'], axis=1)
+    del List['annotation']
+    ExtractNumbers(List)
+
+'''
+Nitheesh Kumar 
+TCS Humain 2019
+Object detection using TESSERACT.
+'''
